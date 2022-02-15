fa169fa3969bcc1b1c47cb9380294f3934dcc5b9
Alejandro Guerrero <69565680+KakuInnovation@users.noreply.github.com>
2022-02-15 04:20:05 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const { dir } = require('console');
+const express = require('express');
+const app = express();
+
+
+app.use(express.static('public'));
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('servidor corriendo');
+})
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/home', (req, res)=>{
+    res.sendFile(__dirname + '/views/home.html');
+})
+
+app.get('/login', (req, res)=>{
+    res.sendFile(__dirname + '/views/login.html');
+})
+
+app.get('/register', (req, res)=>{
+    res.sendFile(__dirname + '/views/register.html');
+})
